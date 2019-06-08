a025a5b926f9f117a0879a12463e62ea9390adb2
Tim Boldt <tim.boldt@gmail.com>
2019-06-08 12:55:49 -0700

    OLED blinky on i2c1

--- Src/main.c
+++ Src/main.c
@@ -20,6 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -30,7 +31,63 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+/** Set Lower Column Start Address for Page Addressing Mode. */
+#define SSD1306_SETLOWCOLUMN 0x00
+/** Set Higher Column Start Address for Page Addressing Mode. */
+#define SSD1306_SETHIGHCOLUMN 0x10
+/** Set Memory Addressing Mode. */
+#define SSD1306_MEMORYMODE 0x20
+/** Set display RAM display start line register from 0 - 63. */
+#define SSD1306_SETSTARTLINE 0x40
+/** Set Display Contrast to one of 256 steps. */
+#define SSD1306_SETCONTRAST 0x81
+/** Enable or disable charge pump.  Follow with 0X14 enable, 0X10 disable. */
+#define SSD1306_CHARGEPUMP 0x8D
+/** Set Segment Re-map between data column and the segment driver. */
+#define SSD1306_SEGREMAP 0xA0
+/** Resume display from GRAM content. */
+#define SSD1306_DISPLAYALLON_RESUME 0xA4
+/** Force display on regardless of GRAM content. */
+#define SSD1306_DISPLAYALLON 0xA5
+/** Set Normal Display. */
+#define SSD1306_NORMALDISPLAY 0xA6
+/** Set Inverse Display. */
+#define SSD1306_INVERTDISPLAY 0xA7
+/** Set Multiplex Ratio from 16 to 63. */
+#define SSD1306_SETMULTIPLEX 0xA8
+/** Set Display off. */
+#define SSD1306_DISPLAYOFF 0xAE
+/** Set Display on. */
+#define SSD1306_DISPLAYON 0xAF
+/**Set GDDRAM Page Start Address. */
+#define SSD1306_SETSTARTPAGE 0XB0
+/** Set COM output scan direction normal. */
+#define SSD1306_COMSCANINC 0xC0
+/** Set COM output scan direction reversed. */
+#define SSD1306_COMSCANDEC 0xC8
+/** Set Display Offset. */
+#define SSD1306_SETDISPLAYOFFSET 0xD3
+/** Sets COM signals pin configuration to match the OLED panel layout. */
+#define SSD1306_SETCOMPINS 0xDA
+/** This command adjusts the VCOMH regulator output. */
+#define SSD1306_SETVCOMDETECT 0xDB
+/** Set Display Clock Divide Ratio/ Oscillator Frequency. */
+#define SSD1306_SETDISPLAYCLOCKDIV 0xD5
+/** Set Pre-charge Period */
+#define SSD1306_SETPRECHARGE 0xD9
+/** Deactivate scroll */
+#define SSD1306_DEACTIVATE_SCROLL 0x2E
+/** No Operation Command. */
+#define SSD1306_NOP 0XE3
+//------------------------------------------------------------------------------
+/** Set Pump voltage value: (30H~33H) 6.4, 7.4, 8.0 (POR), 9.0. */
+#define SH1106_SET_PUMP_VOLTAGE 0X30
+/** First byte of set charge pump mode */
+#define SH1106_SET_PUMP_MODE 0XAD
+/** Second byte charge pump on. */
+#define SH1106_PUMP_ON 0X8B
+/** Second byte charge pump off. */
+#define SH1106_PUMP_OFF 0X8A
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -73,14 +130,7 @@ int main(void)
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  
-
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
-
-  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-
-  /* System interrupt init*/
+  HAL_Init();
 
   /* USER CODE BEGIN Init */
 
@@ -96,19 +146,49 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+  HAL_StatusTypeDef status;
+
+  // Display off
+  do {
+    unsigned char data[] = {0x00, 
+    SSD1306_DISPLAYOFF,
+    SSD1306_SETDISPLAYCLOCKDIV, 0x80,  // the suggested ratio 0x80
+    SSD1306_SETMULTIPLEX, 0x3F,        // ratio 64
+    SSD1306_SETDISPLAYOFFSET, 0x0,     // no offset
+    SSD1306_SETSTARTLINE | 0x0,        // line #0
+    SSD1306_CHARGEPUMP, 0x14,          // internal vcc
+    SSD1306_MEMORYMODE, 0x02,          // page mode
+    SSD1306_SEGREMAP | 0x1,            // column 127 mapped to SEG0
+    SSD1306_COMSCANDEC,                // column scan direction reversed
+    SSD1306_SETCOMPINS, 0x12,          // alt COM pins, disable remap
+    SSD1306_SETCONTRAST, 0x7F,         // contrast level 127
+    SSD1306_SETPRECHARGE, 0xF1,        // pre-charge period (1, 15)
+    SSD1306_SETVCOMDETECT, 0x40,       // vcomh regulator level
+    SSD1306_DISPLAYALLON_RESUME,
+    SSD1306_NORMALDISPLAY,
+    //SSD1306_DISPLAYALLON,
+    SSD1306_DISPLAYON};
+    status = HAL_I2C_Master_Transmit(&hi2c1, 0x3c<<1, data, sizeof(data), 100);
+  } while (status != HAL_OK);
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+    unsigned char data[] = {0x00,  SSD1306_DISPLAYALLON_RESUME};
   while (1)
   {
+    LL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+    for (int i = 0; i < 1000000; i++) {
+      
+    }
+  do {
+    status = HAL_I2C_Master_Transmit(&hi2c1, 0x3c<<1, data, sizeof(data), 100);
+  } while (status != HAL_OK);
+  data[1] ^= 0x01;
     /* USER CODE END WHILE */
-   LL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-   for (int i = 0; i < 1000000; i++) {
-     
-   }
 
     /* USER CODE BEGIN 3 */
   }
@@ -150,6 +230,7 @@ void SystemClock_Config(void)
   LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
   LL_SetSystemCoreClock(16000000);
   LL_RCC_SetUSARTClockSource(LL_RCC_USART2_CLKSOURCE_PCLK1);
+  LL_RCC_SetI2CClockSource(LL_RCC_I2C1_CLKSOURCE_PCLK1);
 }
 
 /* USER CODE BEGIN 4 */
